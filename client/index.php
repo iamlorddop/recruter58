53e097817f16274c35e3a8e49008f9a1ccd70ea8
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-06-16 17:39:38 +0300

    add html template

---
+++ client/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="assets/css/index.css" rel="stylesheet"/>
+	<title>recruter58</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="content">
+			<header class="header">
+				<div class="container"></div>
+			</header>
+			<main class="main">
+				<div class="container"></div>
+			</main>
+		</div>
+		<footer class="footer"></footer>
+	</div>
+</body>
+</html>
