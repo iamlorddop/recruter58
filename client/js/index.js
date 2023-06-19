ed99f6e0e4e99e65efccead7523a7becf28367b5
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-06-19 10:22:49 +0300

    add pulse to buttons

---
+++ client/js/index.js
@@ -0,0 +1,16 @@
+const button = document.getElementsByClassName('btn')
+
+Array.prototype.forEach.call(button, b => b.addEventListener('click', addElement))
+
+function addElement(e) {
+   let addDiv = document.createElement('div'),
+   mValue = Math.max(this.clientWidth, this.clientHeight),
+   rect = this.getBoundingClientRect()
+
+   addDiv.style.width = addDiv.style.height = mValue + 'px'
+   addDiv.style.left = e.clientX - rect.left - (mValue / 2) + 'px'
+   addDiv.style.top = e.clientY - rect.top - (mValue / 2) + 'px'
+
+   addDiv.classList.add('pulse')
+   this.appendChild(addDiv)
+}
